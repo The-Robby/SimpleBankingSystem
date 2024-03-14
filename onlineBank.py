56f81155f402a96c5994cf82935ed81999aa33fb
The-Robby <robbert.groffi@gmail.com>
2024-03-14 04:35:36 +0100

    Moved all functions to main file for efficiency

--- onlineBank.py
+++ onlineBank.py
@@ -1,16 +1,49 @@
-import func
 from flask import Flask, render_template, request, redirect, url_for, session
+from hashlib import pbkdf2_hmac as pbkdf2
+import re
+import os
 
 app = Flask(__name__, static_folder="static")
 app.secret_key = 'your_secret_key'
-
-
 accounts = []
+# -----------------------------------------------------------------------------------------FUNCTIONS--------------------------------------------------------------------------------
+def check_pass_cond(password):
+    pattern = re.compile(r'^(?=.*[a-z])(?=.*\d)(?=.*[A-Z])(?=.*[\W_])[a-zA-Z0-9\W_]{6,12}$')
 
+    if re.match(pattern, password):
+        return True
+    else:
+        return False
+    
+def create_hash(password, salt):
+    plaintext = password.encode()
+    digest = pbkdf2('sha256', plaintext, salt, 100000)
+    hex_hash = digest.hex()
+    return hex_hash
+
+def store_pass(password):
+    salt=os.urandom(32)
+    hashed_password = create_hash(password,salt)
+    return [hashed_password, salt.decode('latin1')]
+
+def check_password(account, userPassword):
+    hash_password_InDB = account['password'][0]
+    salt = account['password'][1].encode('latin1')
+    hex_hash = create_hash(userPassword,salt)
+    return hex_hash == hash_password_InDB
+
+# -----------------------------------------------------------------------------------------INDEX.HTML--------------------------------------------------------------------------------
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/logout")
+def logout():
+    if 'account' in session:
+        session.pop('account')
+    return redirect(url_for('index'))
+
+# -----------------------------------------------------------------------------------------LOGIN.HTML--------------------------------------------------------------------------------
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -19,24 +52,18 @@ def login():
         if username == 'debug':
             return accounts
         for account in accounts:
-            if account['username'] == username and func.check_password(account, password):
+            if account['username'] == username and check_password(account, password):
                 session['account'] = account
                 return redirect(url_for('dashboard'))
         return render_template("login.html", wrongcredentials=True)
     return render_template("login.html")
-
-@app.route("/logout")
-def logout():
-    if 'account' in session:
-        session.pop('account')
-    return redirect(url_for('index'))
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if func.check_pass_cond(password):
+        if check_pass_cond(password):
             check = 1
             for acc in accounts:
                 if acc['username'] == username:
@@ -44,13 +71,14 @@ def register():
                     return render_template("login.html", register=True, wrongname=True, username=username)
             if check == 1:    
                 account_number = len(accounts) + 1
-                accounts.append({'account_number': account_number, 'username': username, 'password': func.store_pass(password), 'balance': 100.0})
+                accounts.append({'account_number': account_number, 'username': username, 'password': store_pass(password), 'balance': 100.0})
                 return redirect(url_for('index'))
         else:
             return render_template("login.html", register=True, wrongpass=True)
 
     return render_template("login.html", register=True)
 
+# -----------------------------------------------------------------------------------------DASHBOARD.HTML--------------------------------------------------------------------------------
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     if 'account' in session:
@@ -58,6 +86,7 @@ def dashboard():
         return render_template('dashboard.html', account=account)
     return redirect(url_for('login'))
 
+# -----------------------------------------------------------------------------------------DEPOSIT.HTML--------------------------------------------------------------------------------
 @app.route('/deposit', methods=['POST'])
 def deposit():
     if 'account' in session:
@@ -95,6 +124,7 @@ def withdraw():
                     return render_template('dashboard.html', account=account, amount=amount, error=True)
     
     return redirect(url_for('login'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
